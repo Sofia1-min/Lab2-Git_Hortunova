61ba4f0f4827ac986125db8f23bf54d965385157
Sofia1-min <sofiahortunova@gmail.com>
2026-09-12 09:50:20 +0300

    Add user name input and greeting

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,9 +1,3 @@
-﻿namespace Program;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
+﻿Console.WriteLine("Enter your name:");
+string? name = Console.ReadLine();
+Console.WriteLine($"Hello, {name}");
